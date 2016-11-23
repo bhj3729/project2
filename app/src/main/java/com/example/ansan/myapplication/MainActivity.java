5b5145ba393156ebd678b2cba3599b3eb46eb5b2
project1 <bhj3729@naver.com>
2016-11-23 19:23:49 +0900

    선언

--- app/src/main/java/com/example/ansan/myapplication/MainActivity.java
+++ app/src/main/java/com/example/ansan/myapplication/MainActivity.java
@@ -2,8 +2,13 @@ package com.example.ansan.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText e1, e2, e3;
+    Button b1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
